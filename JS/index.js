d4ce0fac6a49fa233c0fd8cc8f261a0e5d2269d2
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-21 21:45:45 -0500

    If first number equals 0, cannot decode

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,15 @@
 console.log("Decode!");
 
 const helper = (data, k, storage) => {
+	// Exit code
+	// Return 1 if length equals 0
 	if (k === 0) return 1;
+
+	// If first number equals 0, cannot decode
+	let s = data.length - k;
+
+	if (k[s] === '0') return 0;
+
 }
 
 const numberOfWays = (data) => {
