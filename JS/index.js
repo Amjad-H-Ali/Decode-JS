d65bdb0b4563fd7b4afff63f6447e5bfe4bba260
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-21 21:40:06 -0500

    Defined object to store results

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,11 @@
 console.log("Decode!");
+
+const helper = (data, k, storage) => {
+
+}
+
+const numberOfWays = (data) => {
+	// To store result
+	let storage = {};
+	helper(data, data.length, storage);
+}
