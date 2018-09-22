f7142a67c9b75f827619e03cdc78e94085609dc8
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-09-21 21:59:32 -0500

    Result equals first number decoded + helper(remaining numbers) And first two numbers decoded + helper(remaining numbers) if number <= 27

--- JS/index.js
+++ JS/index.js
@@ -7,8 +7,13 @@ const helper = (data, k, storage) => {
 
 	// If first number equals 0, cannot decode
 	let s = data.length - k;
-
 	if (k[s] === '0') return 0;
+
+	// Result equals first number decoded + helper(remaining numbers)
+	// And first two numbers decoded + helper(remaining numbers) if number <= 27
+	let result = helper(data, k - 1, storage);
+
+	if (k >= 2 && parseInt(data.splice(0, s + 2)) <= 26 ) result += helper(data, k - 2, storage);
 
 }
 
